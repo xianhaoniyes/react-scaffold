626687b671dd2a945a18c54fd72abd7e7b89e0f4
xianhaoni <xianhaoni@gmail.com>
2020-06-22 15:32:49 +0800

    a simple react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyElement = () => <h1>xianhao is learning react!</h1>
+
+ReactDOM.render(<MyElement/>, document.getElementById("root"));
